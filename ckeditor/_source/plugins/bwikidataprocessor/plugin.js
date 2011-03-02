9519dd4f0dba5f5050d9af41f92ce34fda87f6eb
Rob Nagler <github@q33.us>
2011-03-02 12:26:21 +0000

    &copy

--- ckeditor/_source/plugins/bwikidataprocessor/plugin.js
+++ ckeditor/_source/plugins/bwikidataprocessor/plugin.js
@@ -1,9 +1,22 @@
-﻿//
-// Bivio Wiki Language translation
-//
+﻿// Copyright (c) 2001-2011 bivio Software, Inc.  All rights reserved.
 // $Id$
 //
-
+// Visit http://www.bivio.biz for more info.
+// 
+// This library is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library;  If not, you may get a copy from:
+// http://www.opensource.org/licenses/lgpl-license.html
+// 
 function b2hFormat(line, marker, open, close)
 {
     var result = '';
