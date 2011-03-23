e678f124c2a79b43c4dc8ecfe3f17cb689ca3d80
Andrew Schofield <andrews@bivio.biz>
2011-03-23 10:02:08 +0000

    Command line utility to convert between html and bwiki formats

---
+++ ckeditor/t/convert.js
@@ -0,0 +1,44 @@
+// $Id$
+// bwiki <==> html converter for ckeditor unit tests
+
+var plugin = readFile("_source/plugins/bwikidataprocessor/plugin.js");
+
+//
+// Dummy definitions to allow the plugin to be
+// evaluated in this environment
+// 
+
+var window = new Object();
+var document = "unit-test";
+var CKEDITOR = new Object();
+CKEDITOR.tools = new Object();
+CKEDITOR.tools.extend = function() {
+    return '';
+}
+CKEDITOR.dtd = new Object();
+CKEDITOR.dtd.$block = '';
+CKEDITOR.env = new Object();
+CKEDITOR.env.ie = 0;
+CKEDITOR.plugins = new Object();
+CKEDITOR.plugins.add = function() {
+    return '';
+}
+eval(plugin);
+
+var command = arguments[0];
+var filename = arguments[1];
+
+if (!filename || !command.match(/^(-tobwiki|-tohtml)$/)) {
+    print("Usage: {-tobwiki} <input-file>\n" +
+	  "       {-tohtml }\n");
+    quit();
+}
+var input = readFile(filename);
+if (command == "-tohtml") {
+    print(b2h(input) + '\n');
+}
+else {
+    print(h2b(input) + '\n');
+}
+
+
